9944be53565629bb34567ef4c8a05bb90c39cd93
Anton Pawlik <anton.pawlik@gmail.com>
2013-10-29 12:09:40 +0100

    draw routes is better

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 unless Bhf::Engine.config.remove_default_routes
-  Rails.application.routes.append(&Ksk::Engine.config.ksk_routes)
+  Rails.application.routes.draw(&Ksk::Engine.config.ksk_routes)
 end
