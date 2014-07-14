eac37b7c844960ab56a179a936a450a8a9a81dde
Anton Pawlik <anton.pawlik@gmail.com>
2014-07-14 11:03:59 +0200

    abstract settings added

--- lib/ksk.rb
+++ lib/ksk.rb
@@ -2,10 +2,11 @@ module Ksk
   class Engine < Rails::Engine
     
     isolate_namespace Ksk
-    
+
     Bhf.configure do |config|
       config.css << 'ksk/application'
       config.js << 'ksk/application'
+      config.abstract_settings << "#{File.expand_path("../..", __FILE__)}/config/bhf/ksk"
     end
     
     initializer 'ksk.helper' do
